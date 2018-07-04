15836be59c310071bec2525a71b60d3a71f7b1c3
ayin86 <ayin86@163.com>
2018-07-04 22:17:38 +0800

    1st

---
+++ RBB-Html/lib/init/loadFiles.js
@@ -0,0 +1,41 @@
+less = { env: 'development'};
+document.write(
+"<script src='lib/3rd/jquery-1.11.1.min.js'></script>"+
+"<script src='lib/aYin/aYin.js'></script>"+
+"<link rel='stylesheet' type='text/css' href='lib/aYin/aYin.css'>"+
+"<script src='lib/3rd/clipboard/clipboard.js'></script>"+
+//"<script src='lib/3rd/scrollReveal.js'></script>"+
+"<script type='text/javascript' src='lib/3rd/particleground/jquery.particleground.min.js'></script>"+
+"<script src='lib/3rd/jquery.mousewheel.js'></script>"+
+"<script src='lib/3rd/jqScrollbar/jquery.mCustomScrollbar.js'></script>"+
+"<link rel='stylesheet' type='text/css' href='lib/3rd/jqScrollbar/jquery.mCustomScrollbar.css'>"+
+//"<script src='lib/3rd/bootstrap4/js/bootstrap.bundle.min.js'></script>"+
+"<script src='lib/3rd/popper.min.js'></script>"+
+"<script src='lib/3rd/bootstrap4/js/bootstrap.min.js'></script>"+
+"<script src='lib/3rd/scrollReveal.js'></script>"+
+"<link rel='stylesheet' type='text/css' href='lib/3rd/bootstrap4/css/bootstrap.min.css'>"+
+"<link rel='stylesheet' type='text/css' href='lib/3rd/font-awesome/web-fonts-with-css/css/fontawesome-all.min.css'>"+
+//"<link rel='stylesheet' href='https://pro.fontawesome.com/releases/v5.0.10/css/all.css' integrity='sha384-KwxQKNj2D0XKEW5O/Y6haRH39PE/xry8SAoLbpbCMraqlX7kUP6KHOnrlrtvuJLR' crossorigin='anonymous'>"+
+"<link rel='stylesheet' type='text/css' href='lib/style/css/main.css'>"+
+//"<link rel='stylesheet/less' type='text/css' href='lib/style/css/main.less'/>"+
+//"<script src='lib/3rd/less/less.js'></script>"+
+"<script src='lib/3rd/wow/wow.min.js'></script>"+
+/*
+
+"<script src='lib/3rd/bootstrap-datetimepicker/bootstrap-datetimepicker.min.js'></script>"+
+"<script src='lib/3rd/bootstrap-datetimepicker/bootstrap-datetimepicker.zh-CN.js'></script>"+
+"<link rel='stylesheet' type='text/css' href='lib/3rd/bootstrap-datetimepicker/bootstrap-datetimepicker.css'>"+
+
+*/
+""
+); 
+
+
+  
+  
+
+
+
+
+
+
